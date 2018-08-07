794231badb2662e799d94941c6d016db8a3ba3fc
joao.fagundes <joao.fagundes@DSKDEV08>
2018-08-07 13:24:34 -0300

    Primeiro Commit.

---
+++ 01_Task/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace _01_Task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.RichTextBox();
+            this.txtSite = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(319, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Fazer Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(13, 50);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(321, 191);
+            this.txtResultado.TabIndex = 1;
+            this.txtResultado.Text = "";
+            // 
+            // txtSite
+            // 
+            this.txtSite.Location = new System.Drawing.Point(46, 24);
+            this.txtSite.Name = "txtSite";
+            this.txtSite.Size = new System.Drawing.Size(288, 20);
+            this.txtSite.TabIndex = 2;
+            this.txtSite.Text = "http://www.google.com.br";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Site:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 295);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSite);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox txtResultado;
+        private System.Windows.Forms.TextBox txtSite;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 01_Task/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+
+namespace _01_Task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //REDE, ARMAZENAMENTO, TELE/DISPLAY
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //Utilizando task transformamos o fluxo em asynchrono (Thread interna)
+            string endereco = txtSite.Text;
+
+            WebClient web = new WebClient();
+
+            //string html = web.DownloadString(endereco);
+            string html = await web.DownloadStringTaskAsync(new Uri(endereco));    
+
+            txtResultado.Text = html;
+        }
+    }
+}
